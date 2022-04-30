b14e36c99a6c11f51a390153df5040e0d1cd4f24
Seong-Yong <leek6789@naver.com>
2022-04-30 22:40:36 +0900

    Feat: 탑다운 맵 플레이어 이동 구현

--- printf_HelloGachon/Assets/Script/TopViewPlayer.cs
+++ printf_HelloGachon/Assets/Script/TopViewPlayer.cs
@@ -8,15 +8,64 @@ public class TopViewPlayer : MonoBehaviour
     float h;
     float v;
     Rigidbody2D rigid;
+    public float speed;
+    bool isHorizonMove;
+    private Animator anim;
+    Vector3 dirVec;
+    GameObject scanObject;
     void Awake(){
         rigid=GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
     }
     void Update(){
-        h=Input.GetAxis("Horizontal");
-        v=Input.GetAxis("Vertical");
+        h=Input.GetAxisRaw("Horizontal");
+        v=Input.GetAxisRaw("Vertical");
+        bool hDown=Input.GetButtonDown("Horizontal");
+        bool vDown=Input.GetButtonDown("Vertical");
+        bool hUp=Input.GetButtonUp("Horizontal");
+        bool vUp=Input.GetButtonUp("Vertical");
+        if(hDown)
+            isHorizonMove=true;
+        else if(vDown)
+            isHorizonMove=false;
+        else if(hUp||vUp)
+            isHorizonMove=h !=0;
+        if(anim.GetInteger("hAxisRaw")!=h){
+            anim.SetBool("isChange",true);
+            anim.SetInteger("hAxisRaw",(int)h);
+        }
+        else if(anim.GetInteger("vAxisRaw")!=v){
+            anim.SetBool("isChange",true);
+            anim.SetInteger("vAxisRaw",(int)v);
+        }
+        else
+            anim.SetBool("isChange",false);
+        if(vDown && v==1)
+            dirVec=Vector3.up;
+        else if(vDown && v==-1)
+            dirVec=Vector3.down;
+        else if(hDown && h==-1)
+            dirVec=Vector3.left;
+        else if(hDown && h==1)
+            dirVec=Vector3.right;
+        
+        if(Input.GetButtonDown("Jump")&&scanObject!=null)
+            Debug.Log(scanObject.name);
+        
+        
     }
     void FixedUpdate() {
-        rigid.velocity=new Vector2(h,v);
+        Vector2 moveVec=isHorizonMove?new Vector2(h,0):new Vector2(0,v);
+        rigid.velocity=moveVec*speed;
+
+        Debug.DrawRay(rigid.position,dirVec*0.7f,new Color(0,1,0));
+        RaycastHit2D rayHit=Physics2D.Raycast(rigid.position,dirVec,0.7f,LayerMask.GetMask("Object"));
+
+        if(rayHit.collider!=null){
+            scanObject=rayHit.collider.gameObject;
+        }
+        else
+            scanObject=null;
         
     }
 }
