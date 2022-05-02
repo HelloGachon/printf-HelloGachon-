d764c813208425c77c5359f334ca13a30b3bd7e8
Seokyung Jee <48540451+Seokyung@users.noreply.github.com>
2022-05-03 07:23:08 +0900

    Feat: 수강신청게임 로직구현 완료
    추가적인 필요작업: 1.수강신청 성공 실패여부에 따라 시간표 다르게 부여 2.시간표 부여 후 다음 씬으로 이동

--- printf_HelloGachon/Assets/MiniGame1/Scripts/TypeGameManager.cs
+++ printf_HelloGachon/Assets/MiniGame1/Scripts/TypeGameManager.cs
@@ -1,48 +1,143 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TypeGameManager : MonoBehaviour
 {
+    //**타이머 변수들 선언
+    public string remainingTime = @"00:00:000";
+    private bool isPlaying = true;
+    public Text timerTxt;
+    public float setTime; //inspecter 창에서 초를 입력받음
+    private float getSetTime;
+
+    //**수강신청 게임 변수들 선언
     public GameObject Register;
     public GameObject registerResult;
-    private Timer timer;
+    public List<ClassNum> classNumList;
     public Text classNumTxt;
     public Text resultTxt;
     public InputField inputClassNum;
     public Button applyBtn;
     public Button okBtn;
+    public bool isApplyBtn = false;
+    public int currentClassNum;
+    public int successCnt = 0;
+    private int loop = 0;
 
-    //test용 더미데이터
-    private string classNum1 = "06823001";
-    //private string classNum2 = "14329002";
-
-    void Update()
+    void Start()
     {
-        classNumTxt.text = classNum1;
+        /*
+        ** [구현해야 할 것]
+        ** 3번 이상 성공시 성공시간표, 아니면 실패 시간표 부여
+        */
+
+        displayClassNum();
+        getSetTime = setTime;
+        resetTimer();
+    }
+
+    void Update() {
+        if(loop < 6)
+        {
+            if(isPlaying) {
+                remainingTime = CountdowmTimer();
+            }
+
+            if(setTime < 0 && !isApplyBtn)
+            {
+                setZero();
+                resultTxt.text = "타임오버!";
+                registerResult.SetActive(true);
+                classNumList.RemoveAt(currentClassNum);
+            }
+
+            if(isApplyBtn)
+            {
+                setZero();
+                if(inputClassNum.text == classNumList[currentClassNum].classNum)
+                {
+                    resultTxt.text = "성공!";
+                    successCnt++;
+                }
+                else
+                {
+                    resultTxt.text = "실패!";   
+                }
+
+                registerResult.SetActive(true);
+                classNumList.RemoveAt(currentClassNum);
+                isApplyBtn = false;           
+            }        
+            
+            if(timerTxt) {
+                timerTxt.text = remainingTime;
+            }
+        }
+        else
+        {
+            classNumTxt.text = "";
+            if(successCnt > 3)
+            {
+                resultTxt.text = "수강신청 성공!\n" + "성공 횟수 : " + successCnt;
+            }
+            else
+            {
+                resultTxt.text = "수강신청 실패..\n" + "성공 횟수 : " + successCnt;
+            }
+            registerResult.SetActive(true);
+        }
+    }
+
+    //**타이머 로직
+    private string CountdowmTimer(bool isUpdate = true)
+    {
+        if(isUpdate) {
+            setTime -= Time.deltaTime;
+        }
+
+        TimeSpan timeSpan = TimeSpan.FromSeconds(setTime);
+        string timer = string.Format("{0:00}:{1:00}:{2:000}", timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+        return timer;
+    }
+
+    void resetTimer()
+    {
+        if(loop < 6)
+        {            
+            registerResult.SetActive(false);
+            setTime = getSetTime;
+            remainingTime = CountdowmTimer(true);            
+            isPlaying = true;
+            loop++;
+            displayClassNum();
+        }
+        else
+        {
+            //다음 씬으로 넘어가기
+            Debug.Log("게임종료.");
+        }        
+    }
+
+    void setZero()
+    {
+        remainingTime = @"00:00:000";
+        setTime = 0;
+        isPlaying = false;
+    }
+
+    //**수강신청 게임 로직
+    void displayClassNum()
+    {
+        currentClassNum = UnityEngine.Random.Range(0, classNumList.Count);
+        classNumTxt.text = classNumList[currentClassNum].className + ": " + classNumList[currentClassNum].classNum;
+        //classNumList.RemoveAt(currentClassNum);
     }
     
     public void ApplyBtnClick()
     {
-        //timer.setTime = 0.0f;
-        if(inputClassNum.text == classNum1)
-        {
-            //timer.setTime = 0.0f;
-            //timer.DisplayTime(timer.setTime);
-            resultTxt.text = "성공!";
-        }
-        else if (inputClassNum.text != classNum1)
-        {
-            //timer.setTime = 0.0f;
-            //timer.DisplayTime(timer.setTime);
-            resultTxt.text = "실패!";            
-        }
-        registerResult.SetActive(true);
-    }
-    public void checkResult()
-    {
-        registerResult.SetActive(false);
-        Debug.Log("확인완료");
+        isApplyBtn = true;         
     }
 }
