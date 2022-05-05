189899b6f320a7529ff5a7f3dcb8254308401841
stjae <leestjae@gmail.com>
2022-05-05 11:25:17 +0900

    Feat: 문답 및 scene 종료 구현

--- printf_HelloGachon/Assets/MiniGame3/Scripts/DialManager.cs
+++ printf_HelloGachon/Assets/MiniGame3/Scripts/DialManager.cs
@@ -5,40 +5,38 @@ using UnityEngine.UI;
 
 public class DialManager : MonoBehaviour
 {
-    public List<string> dialList;
+    public Dictionary<int, string> dialDict;
 
     void Awake()
     {
-        dialList = new List<string>();
+        dialDict = new Dictionary<int, string>();
         CreateData();
     }
 
     void CreateData()
     {
-        dialList.Add("오늘은 함수에 대해 배워볼거야");
-        dialList.Add("함수는 입력한 값을 처리해서 되돌려주는, 프로그램을 구성하는 가장 작은 단위라고 할 수 있어");
-        dialList.Add("지금 보이는 함수는 정수 값을 입력받아 그 값에 1을 더해 돠돌려주는 함수야");
-        dialList.Add("함수 이름은 addValue 이고");
-        dialList.Add("정수로 입력 받은 값의 이름은 value, 이름 앞에 붙은 int는 이 값이 정수임을 뜻하지");
-        dialList.Add("참고로 이렇게 함수가 입력받는 값을 인수 혹은 파라미터(parameter)라고 해");
-        dialList.Add("{} 안에 작성된 내용은 이 함수가 입력받은 값을 어떻게 처리할지를 보여줘");
-        dialList.Add("value 에 1을 더한 값을 다시 value 에 집어넣었어 이러면 입력받은 value 에 1이 더해진 상태지?");
-        dialList.Add("함수에서 처리한 값을 되돌려줄 때는 return 과 함께 뒤에 돌려주고 싶은 값을 입력해주면 돼");
-        dialList.Add("아, 그리고 코드의 마지막에는 꼭 세미콜론(;)을 붙이는 걸 잊지마");
-        dialList.Add("함수의 이름 앞에 붙은 int 는 함수가 되돌려주는 값이 정수라는 뜻이야");
-        dialList.Add("자, 설명을 들었으니 한 번 직접 함수를 작성해보자");
-        dialList.Add("실수(float)형 값을 입력 받아 1을 뺀 후 되돌려주는 subValue 라는 이름의 함수를 작성해보자");
-        dialList.Add("함수를 작성했다면 우측 상단의 실행 버튼을 누르렴");
-        // dialList.Add("설명한 함수 아래에 main이라는 함수가 있지?");
-        // dialList.Add("main 함수는 프로그램이 실행될 때 가장 처음으로 실행되는 함수이자 꼭 실행되는 함수야");
-        // dialList.Add("main 함수가 끝나면 프로그램도 종료되게 돼");
-        // dialList.Add("main 함수가 printValue 함수를 실행하도록 코드를 작성하고 인수로 1 을 입력했어");
-        // dialList.Add("마지막에 보이는 return 0; ")
-        // dialList.Add("자 이제 프로그램을 실행해 보자, 우측 상단의 실행 버튼을 눌러봐");
+        dialDict.Add(0, "오늘은 함수에 대해 배워볼거야");
+        dialDict.Add(1, "함수는 입력한 값을 처리해서 되돌려주는, 프로그램을 구성하는 가장 작은 단위라고 할 수 있어");
+        dialDict.Add(2, "지금 보이는 함수는 <color=blue>정수</color> 값을 입력받아 그 값에 <color=blue>1</color>을 더해 되돌려주는 <color=orange>addValue</color>라는 함수야");
+        dialDict.Add(3, "함수 이름은 내가 임의로 지었어. 참고로 함수와 입력값의 이름은 맘대로 지어줄 수 있어");
+        dialDict.Add(4, "정수로 입력 받은 값의 이름은 <color=orange>value</color>, 이름 앞에 붙은 <color=blue>int</color>는 이 값이 정수임을 뜻하지");
+        dialDict.Add(5, "참고로 이렇게 함수가 입력받는 값을 인수 혹은 파라미터(parameter)라고 해");
+        dialDict.Add(6, "<color=orange>{}</color> 안에 작성된 내용은 이 함수가 입력받은 값을 어떻게 처리할지를 보여줘");
+        dialDict.Add(7, "함수에서 처리한 값을 되돌려줄 때는 <color=orange>return</color> 과 함께 뒤에 돌려주고 싶은 값을 입력해주면 돼");
+        dialDict.Add(8, "아, 그리고 코드의 마지막에는 꼭 <color=orange>세미콜론</color>(;)을 붙이는 걸 잊지마");
+        dialDict.Add(9, "함수의 이름 앞에 붙은 int 는 함수가 되돌려주는 값이 정수라는 뜻이야");
+        dialDict.Add(10, "자, 설명을 들었으니 한 번 직접 함수를 작성해보자");
+        dialDict.Add(11, "이름이 fValue인 실수(float)형 값을 입력 받아 1을 뺀 후 되돌려주는 subValue 라는 이름의 함수를 작성하고 대화창을 클릭하렴");
+        dialDict.Add(12, "훌륭해! 오늘 수업은 여기까지다");
     }
 
     public string GetDial(int index)
     {
-        return dialList[index];
+        return dialDict[index];
+    }
+
+    public int GetSize()
+    {
+        return dialDict.Count;
     }
 }
--- printf_HelloGachon/Assets/MiniGame3/Scripts/GameManager.cs
+++ printf_HelloGachon/Assets/MiniGame3/Scripts/GameManager.cs
@@ -8,11 +8,107 @@ public class GameManager : MonoBehaviour
     public DialManager dialManager;
     public Button dialBtn;
     public Text dialText;
+    public InputField funcType;
+    public InputField funcName;
+    public InputField paramType;
+    public InputField paramName;
+    public InputField funcBody;
+    public Color white;
+    public Color orange;
+    public Color yellow;
     int count = 0;
 
     public void BtnOnClick()
     {
-        dialText.text = dialManager.GetDial(count);
+        white = new Color (1.0f, 1.0f, 1.0f, 1.0f);
+        orange = new Color (1.0f, 0.64f, 0.0f, 1.0f);
+        yellow = new Color (0.95f, 0.95f, 0.1f, 1.0f);
+
+        int size = dialManager.GetSize();
+
+        if (count > size - 1)
+        {
+            ExitGame();
+        } else if (count <= size - 1)
+        {
+            dialText.text = dialManager.GetDial(count);
+        }
+
+        Debug.Log(count);
+
+        switch(count)
+        {
+            case 2:
+            funcName.GetComponent<Image>().color = orange;
+            funcType.text = "int";
+            funcName.text = "addValue";
+            paramType.text = "int";
+            paramName.text = "value";
+            funcBody.text = "return value + 1;";
+            break;
+
+            case 3:
+            break;
+
+            case 4:
+            funcName.GetComponent<Image>().color = white;
+            paramType.GetComponent<Image>().color = orange;
+            paramName.GetComponent<Image>().color = orange;
+            break;
+
+            case 6:
+            paramType.GetComponent<Image>().color = white;
+            paramName.GetComponent<Image>().color = white;
+            funcBody.GetComponent<Image>().color = orange;
+            break;
+
+            case 9:
+            funcBody.GetComponent<Image>().color = white;
+            funcType.GetComponent<Image>().color = orange;
+            break;
+
+            case 10:
+            funcType.GetComponent<Image>().color = white;
+            funcType.text = "";
+            funcName.text = "";
+            paramType.text = "";
+            paramName.text = "";
+            funcBody.text = "";
+            break;
+
+            // case 11:
+            // funcType.interactable = true;
+            // funcName.interactable = true;
+            // paramType.interactable = true;
+            // paramName.interactable = true;
+            // funcBody.interactable = true;
+            // break;
+
+            case 12:
+            string funcBodySpaceRemove = funcBody.text.Replace(" ","");
+            string[] returnStatSplit = funcBody.text.Split('-');
+
+            if (funcType.text.Trim() == "float" && funcName.text.Trim() == "subValue" && paramType.text.Trim() == "float"
+            && paramName.text.Trim() == "fValue" && returnStatSplit[0].Trim() == "return fValue"
+            && funcBodySpaceRemove.Trim() == "returnfValue-1;")
+            {
+                break;
+            } else
+            {
+                dialText.text = "흠.. 코드를 다시 한 번 살펴볼까?";
+                count -= 2;
+            }
+            break;
+        }
         count++;
+    }
+
+    void ExitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
     }
 }
